d827d0192a1ff16fbec3170a559d3a24a034c8ca
Gabriel Rodrigues <106823036+Gabriel3527@users.noreply.github.com>
2023-11-22 12:00:25 -0300

    s

--- main.js
+++ main.js
@@ -1,9 +1,45 @@
 function tocaSomPom (){
-    document.querySelector('#som_tecla_pom').play();
-    }
-    document.querySelector('.tecla_pom') .onclick = tocaSomPom;
+document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom') .onclick = tocaSomPom;
 
-    function tocaSomclap (){
-        document.querySelector('#som_tecla_clap').play();
-        }
-        document.querySelector('.tecla_clap') .onclick = tocaSomPom;
+function tocaSomclap (){
+document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap') .onclick = tocaSomclap;
+
+function tocaSomtim (){
+document.querySelector('#som_tecla_tim').play();
+}
+document.querySelector('.tecla_tim') .onclick = tocaSomtim;
+
+function tocaSompuff (){
+document.querySelector('#som_tecla_puff').play();
+}
+document.querySelector('.tecla_puff') .onclick = tocaSompuff;
+
+function tocaSomsplash (){
+document.querySelector('#som_tecla_splash').play();
+}
+document.querySelector('.tecla_splash') .onclick = tocaSomsplash;
+
+function tocaSomtoin (){
+document.querySelector('#som_tecla_toim').play();
+}
+document.querySelector('.tecla_toim') .onclick = tocaSomtoim;
+    
+    
+function tocaSomPsh (){
+document.querySelector('#som_tecla_psh').play();
+}
+document.querySelector('.tecla_psh') .onclick = tocaSompsh;
+
+function tocaSomtic (){
+document.querySelector('#som_tecla_tic').play();
+}
+document.querySelector('.tecla_tic') .onclick = tocaSomtic;
+
+function tocaSomtom (){
+document.querySelector('#som_tecla_tom').play();
+}
+document.querySelector('.tecla_tom') .onclick = tocaSomtom;
